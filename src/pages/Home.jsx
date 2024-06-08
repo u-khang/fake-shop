fef3e7b08a98a71438e0851958c826b493a72d3e
u-khang <khang.nguyen8675@gmail.com>
2024-06-08 14:33:38 -0400

    add testing data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,10 @@ function Home() {
             <h1>Welcome to Sugar!</h1>
             <p>We like eating desserts</p>
             <p>You can have some too!</p>
-            <Button link='/shop' text='SHOP NOW'/>
+            <Button 
+                link='/shop' 
+                text='SHOP NOW'
+            />
         </>
     )
 }
