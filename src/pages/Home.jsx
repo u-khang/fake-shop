ed5d80a9f02ed4243bb8e513fa4654053f309506
u-khang <khang.nguyen8675@gmail.com>
2024-06-10 19:32:10 -0400

    setup tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
         <>
             <h1>Welcome to Sugar!</h1>
             <p>We like eating desserts</p>
-            <p>You can have some too!</p>
+            <p className="text-3xl font-bold underline">You can have some too!</p>
             <Button 
                 link='/shop' 
                 text='SHOP NOW'
