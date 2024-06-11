7064bc91062d676a1bf250d1a79db94763c2b76f
u-khang <khang.nguyen8675@gmail.com>
2024-06-11 16:49:23 -0400

    change to mobile first responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,49 @@
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/Sweets_Club_Icon.webp'
-import font from '../assets/Pacifico/Pacifico-Regular.ttf'
+import { useState } from 'react';
+
 function Nav() {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+      setOpenMenu(!openMenu);
+    };
+  
+    const closeMenu = () => {
+      setOpenMenu(false);
+    };
+
     return (
         <div 
             id="nav"
-            className='fixed left-0 top-0 flex justify-between w-full border-b-2 border-pink-500 bg-pink-300 p-4 px-36 text-2xl'
+            className='fixed left-0 top-0 flex justify-between w-full p-1 border-b-2 border-pink-500 bg-pink-300 text-2xl'
         >
             <Link to="/" >
-            <div className='flex'>
-                <img src={logo} alt="sugar-logo" className='w-20'/>
-                <h1 className='m-auto font-Pacifico text-4xl text-yellow-950'>Sweets Club</h1>
-            </div>
+                <div className='flex'>
+                    <img src={logo} alt="sugar-logo" className='w-20'/>
+                    <h1 className='m-auto font-Pacifico text-2xl text-yellow-950'>Sweets Club</h1>
+                </div>
             </Link>
-            <nav className='w-52'>
-                <ul className='flex h-full justify-between'>
-                    <li className='m-auto hover:underline focus-visible:underline'>
+
+            <button
+                className="absolute right-6 top-6 scale-125 sm:hidden"
+                onClick={toggleMenu}
+            >
+                <span className="material-symbols-outlined">
+                    {openMenu ? "close" : "menu"}
+                </span>
+            </button>
+
+            <nav className={`absolute left-0 top-full z-10 h-48 w-full bg-inherit p-6 ${openMenu ? "border-b-2 border-pink-500" : "hidden"}`}>
+                <ul className='flex flex-col sm:flex-row sm:gap-10'>
+                    <li className='p-6 text-center text-2xl hover:underline focus-visible:underline'>
                         <NavLink 
                             to="/shop"
                         >
                             <p>Shop</p>
                         </NavLink>
                     </li>
-                    <li className='m-auto hover:underline focus-visible:underline'>
+                    <li className='p-6 text-center text-2xl hover:underline focus-visible:underline'>
                         <NavLink
                             to="/cart" 
                         >
