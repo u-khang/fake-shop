55d3295f632aeec4a968807390f312cf1833ee72
u-khang <khang.nguyen8675@gmail.com>
2024-06-11 13:24:01 -0400

    add styles to Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,18 @@ import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/Sweets_Club_Icon.webp'
 function Nav() {
     return (
-        <div id="nav">
-            <Link to="/">
-                <img src={logo} alt="sugar-logo" />
+        <div 
+            id="nav"
+            className='fixed left-0 top-0 flex justify-between w-full border-b-2 border-pink-500 bg-pink-300 p-4 px-36'
+        >
+            <Link to="/" >
+            <div className='flex'>
+                <img src={logo} alt="sugar-logo" className='w-20'/>
+                <h1>Sweets Club</h1>
+            </div>
             </Link>
-            <nav>
-                <ul>
+            <nav className='w-52'>
+                <ul className='flex justify-between'>
                     <li>
                         <NavLink 
                             to="/shop"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ import Button from '../components/Button'
 
 function Home() {
     return (
-        <>
+        <div>
             <h1>Welcome to Sugar!</h1>
             <p>We like eating desserts</p>
-            <p className="text-3xl font-bold underline">You can have some too!</p>
+            <p>You can have some too!</p>
             <Button 
                 link='/shop' 
                 text='SHOP NOW'
             />
-        </>
+        </div>
     )
 }
 
