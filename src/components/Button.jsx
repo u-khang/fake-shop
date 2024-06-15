62a3b94e21a1624e8e054e32d42c8206a691c736
u-khang <khang.nguyen8675@gmail.com>
2024-06-15 15:07:07 -0400

    change Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 
 function Button({text, link = '', onClick = ''}) {
+    if(link) {
         return (
-            <Link to={link} onClick={onClick}>
+            <Link to={link}>
                 <button className="">
                     <p>{text}</p>
                 </button>
             </Link>
         )
+    }
+    else {
+        return (
+            <button onClick={onClick}>
+                <p>{text}</p>
+            </button>
+        )
+    }
 }
 
 export default Button
