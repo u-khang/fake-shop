466d96685a1155e2185983c222c93656a7ace26a
u-khang <khang.nguyen8675@gmail.com>
2024-06-18 12:51:13 -0400

    finishing Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,20 +17,20 @@ function Nav() {
         <div 
             id="nav"
             // className='z-10 fixed left-0 top-0 flex justify-between w-full p-1 border-b-2 border-pink-700 bg-pink-300 text-2xl h-44'
-            className="fixed z-10 w-full h-20 bg-white"
+            className="fixed z-10 w-full h-20 bg-red-50 sm:h-28"
         >   
             <div className='flex h-full p-3'>
-                <div className='w-1/6 m-auto'>
-                <span class="material-symbols-outlined">
-                    search
-                </span>
+                <div className='w-1/4 m-auto'>
+                    <span class="material-symbols-outlined ml-6">
+                        search
+                    </span>
                 </div>
 
-                <div className='w-3/6'>
+                <div className='w-2/4'>
                     <Link to="/" >
-                        <div className='flex h-full'>
+                        <div className='flex h-full w-40 sm:w-72 m-auto'>
                             <img src={logo} alt="sugar-logo" className=''/>
-                            <h1 className='m-auto font-Pacifico text-xl'
+                            <h1 className='m-auto font-Pacifico text-xl text-rose-950 sm:text-3xl'
                                 // className='m-auto font-Pacifico text-2xl text-yellow-950 '
                             >Sweets Club
                             </h1>
@@ -40,21 +40,32 @@ function Nav() {
 
                 <button
                     // className="absolute right-6 top-8 scale-125 sm:hidden"
-                    className='w-2/6'
+                    className='w-1/4 md:hidden'
                     onClick={toggleMenu}
                 >
                     <span className="material-symbols-outlined">
                         {openMenu ? "close" : "menu"}
                     </span>
                 </button>
+                <div className='hidden md:block w-1/4 flex m-auto'>
+                    <button className='w-1/2'>
+                        <span className="material-symbols-outlined">person</span>
+                    </button>
+                    <button className='w-1/2'>
+                        <NavLink to='/cart'>
+                            <span className="material-symbols-outlined">shopping_cart</span>
+                        </NavLink>
+                    </button>
+                </div>
             </div>
             <div>
                 <nav 
                     // className={`absolute left-0 top-full h-48 w-full bg-inherit p-6 sm:static sm:block sm:h-auto sm:w-auto sm:p-0 sm:pr-20 ${openMenu ? "border-b-2 border-pink-700" : "hidden"}`}
-                    className={`${openMenu ? "border-b-2 bg-white" : "hidden"}`}
+                    className={`${openMenu ? "border-b-2 bg-red-50" : "hidden"}`}
                 >
                     <ul 
                         // className='flex flex-col sm:flex-row sm:gap-10'
+                        className='text-slate-600 font-bold tracking-wider text-xs'
                     >
                         <li 
                             // className='p-6 text-center text-2xl hover:underline'
@@ -62,17 +73,17 @@ function Nav() {
                             <NavLink 
                                 to="/shop"
                             >
-                                <p>All</p>
+                                <p className='p-4 border-y-2'>Cakes</p>
                             </NavLink>
                         </li>
                         <li>
                             <NavLink>
-                                <p>Pastries</p>
+                                <p className='p-4 border-b-2'>Pastries</p>
                             </NavLink>
                         </li>
                         <li>
                             <NavLink>
-                                <p>Drinks</p>
+                                <p className='p-4 border-b-2'>Drinks</p>
                             </NavLink>
                         </li>
                         <li 
@@ -81,15 +92,34 @@ function Nav() {
                             <NavLink
                                 to="/cart" 
                             >
-                                <p>Cart</p>
+                                <p className='p-4 border-b-2'>Cart</p>
                             </NavLink>
                         </li>
                         <li>
                             <NavLink>
-                                <p>User</p>
+                                <p className='p-4'>User</p>
                             </NavLink>
                         </li>
                     </ul>
+                </nav>
+                <nav className='hidden md:block bg-red-50 h-14'>
+                    <div className='w-80 flex m-auto pt-3 text-slate-600 font-bold tracking-wider text-center'>
+                        <div className='w-1/3'>
+                            <NavLink to='/shop'>
+                                <p className=''>Cakes</p>
+                            </NavLink>
+                        </div>
+                        <div className='w-1/3'>
+                            <NavLink>
+                                <p className=''>Pastries</p>
+                            </NavLink>
+                        </div>
+                        <div className='w-1/3'>
+                            <NavLink>
+                                <p className=''>Drinks</p>
+                            </NavLink>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </div>
