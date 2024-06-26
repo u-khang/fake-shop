aae36c3e64d2a78f105963eff7f7cdab2adcf1fc
u-khang <khang.nguyen8675@gmail.com>
2024-06-26 16:11:15 -0400

    finishing up everything

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,10 @@
 function Checkout() {
-    return <p>CHECKOUT</p>
+    return (
+        <div className="px-4 pb-8 md:mx-28">
+            <div className='h-24 sm:h-32 md:h-48'></div>
+            <p className="mt-20 text-4xl w-full p-auto text-center">This function has not been implemented</p> 
+        </div>
+    )
 }
 
 export default Checkout
